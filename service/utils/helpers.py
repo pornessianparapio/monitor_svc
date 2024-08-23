46d0be6c2d68af577ea9b004d138fe6500db413e
pornessianparapio <bargatpurvesh.hsm1@gmail.com>
2024-08-23 11:09:06 +0530

    added folder utils

---
+++ service/utils/helpers.py
@@ -0,0 +1,13 @@
+import socket
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip_address = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip_address
+
+if __name__ == "__main__":
+    print(get_ip_address())
